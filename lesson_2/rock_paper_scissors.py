405d9e22f78a9e0de1458a14447b069675ebddaf
Conor Driscoll <cdriscoll40@gmail.com>
2025-05-14 17:24:10 -0700

    incorporated remaining RPS bonus features

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -4,22 +4,75 @@ import random
 
 VALID_CHOICES = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
+
 def prompt(message):
     print(f"==> {message}")
 
 def player_choice_input():
     while True:
-        prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
+        prompt(f'Choose one: {", ".join(VALID_CHOICES)}\n')
+        prompt('(feel free to abbreviate your choice, in which case the '
+               'program will use the first letter of your entry to select a '
+               'choice that begins with that same first letter)\n')
         choice = input()
-        title_choice = choice.title()
+        choice_upper = choice.upper()
 
-        if title_choice not in VALID_CHOICES:
+        if choice.title() in VALID_CHOICES:
+            return choice.title()
+
+        os.system('clear')
+
+        final_choice_or_none = check_1st_letter_match(choice, choice_upper)
+
+        if final_choice_or_none:
+            return final_choice_or_none
+
+        os.system('clear')
+        prompt(f"Invalid choice: '{choice}'")
+
+def check_1st_letter_match(choice_argument, choice_upper_argument):
+    valid_choice_matches = [valid_choice for valid_choice in VALID_CHOICES
+                 if valid_choice.startswith(f'{choice_upper_argument[0]}')]
+
+    qty_valid_choice_matches = len(valid_choice_matches)
+
+    if qty_valid_choice_matches == 1:
+        return valid_choice_matches[0]
+
+    if qty_valid_choice_matches > 1:
+        return choose_from_multiple_matches(valid_choice_matches,
+                                            choice_argument)
+
+def choose_from_multiple_matches(valid_choice_matches_argument,
+                                 choice_argument):
+    while True:
+        prompt(f"The first letter of your initial entry, "
+               f"'{choice_argument[0]}', matched the following choices: "
+               f'{", ".join(valid_choice_matches_argument)}.\n')
+        prompt("Please select '1' for the first option listed, '2' for the "
+               "second, etc.\n")
+        index_input = input()
+
+        try:
+            index_int = int(index_input)
+        except ValueError:
             os.system('clear')
-            prompt(f"Invalid choice: {choice}")
+            prompt(f"Invalid selection: '{index_input}'. Please re-select.")
+            continue
         else:
-            return title_choice
+            if index_int < 1:
+                os.system('clear')
+                prompt(f"Invalid selection: '{index_input}'. "
+                       "Please re-select.")
+                continue
 
-def display_winner(both_choices_argument):
+        try:
+            return valid_choice_matches_argument[index_int - 1]
+        except IndexError:
+            os.system('clear')
+            prompt(f"Invalid selection: '{index_input}'. Please re-select.")
+
+def decide_game_winner(both_choices_argument):
 
     os.system('clear')
 
@@ -33,17 +86,30 @@ def display_winner(both_choices_argument):
              ("Spock", "Scissors") | ("Lizard", "Spock") | ("Paper", "Spock") |
              ("Scissors", "Lizard") | ("Rock" , "Lizard")):
 
-            prompt("You win!\n")
+            return "You win the game!"
 
         case (("Rock", "Paper") | ("Rock", "Spock") | ("Paper", "Lizard") |
              ("Paper", "Scissors") | ("Scissors", "Rock") |
              ("Scissors", "Spock") | ("Spock", "Lizard") | ("Spock", "Paper") |
              ("Lizard", "Scissors") | ("Lizard", "Rock")):
 
-            prompt("Computer wins!\n")
+            return "Computer wins the game!"
 
         case _:
-            prompt("It's a tie!\n")
+            return "It's a tie!"
+
+def score_board(game_outcome_argument, player_score_argument,
+              computer_score_argument):
+
+    prompt(f"{game_outcome_argument}\n")
+    prompt(f"Your score is: {player_score_argument} win(s).")
+    prompt(f"The computer's score is: {computer_score_argument} win(s).\n")
+
+    if player_score_argument == 3:
+        prompt("You win the best of five series!\n")
+
+    if computer_score_argument == 3:
+        prompt("Computer wins the best of five series!")
 
 def play_again_input():
     while True:
@@ -59,15 +125,17 @@ def play_again_input():
         if answer_lower.startswith('y'):
             return True
 
-        prompt(f"Invalid choice: {answer}")
+        prompt(f"Invalid choice: '{answer}'")
 
 
 os.system('clear')
 
-prompt(f'Welcome to the {", ".join(VALID_CHOICES)} program!')
+prompt(f'Welcome to the {", ".join(VALID_CHOICES)}, best of 5 program!')
 prompt('Game on!\n')
 
 play_again = 'set to arbitrary truthy value for initial while loop entry'
+player_score = 0
+computer_score = 0
 
 while play_again:
 
@@ -77,6 +145,53 @@ while play_again:
 
     both_choices = (player_choice, computer_choice)
 
-    display_winner(both_choices)
+    game_outcome = decide_game_winner(both_choices)
+
+    match game_outcome:
+        case "You win the game!":
+            player_score += 1
+        case "Computer wins the game!":
+            computer_score += 1
+
+    score_board(game_outcome, player_score,
+              computer_score)
+
+    if player_score < 3 and computer_score < 3:
+        continue
 
     play_again = play_again_input()
+
+
+# Extra unused code below:
+    # Doesn't work:
+        # first_letter_match_counter = 0
+
+        # for valid_choice in VALID_CHOICES:
+        #     if valid_choice.startswith(f'{choice_upper[0]}'):
+        #         selection = valid_choice
+        #         first_letter_match_counter += 1
+
+        # print(first_letter_match_counter)
+
+        # if first_letter_match_counter == 1:
+        #     return selection
+
+        # if first_letter_match_counter == 2:
+        #     for valid_choice in VALID_CHOICES:
+        #         if valid_choice.startswith(f'{choice_upper[0:2]}'):
+        #             return valid_choice
+
+        # This works:
+        # if choice_lower.startswith('r'):
+        #     return 'Rock'
+        # elif choice_lower.startswith('p'):
+        #     return 'Paper'
+        # elif choice_lower.startswith('l'):
+        #     return 'Lizard'
+        # elif choice_lower.startswith('sc'):
+        #     return 'Scissors'
+        # elif choice_lower.startswith('sp'):
+        #     return 'Spock'
+        # else:
+        #     os.system('clear')
+        #     prompt(f"Invalid choice: {choice}")
